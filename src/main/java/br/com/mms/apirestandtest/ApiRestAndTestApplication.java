0abbe2ac2131942b8f92739ece18c914251bc26d
Mario Sclavo <mmsclavo@gmail.com>
2024-03-11 15:44:58 -0300

    commit inicial

---
+++ src/main/java/br/com/mms/apirestandtest/ApiRestAndTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.mms.apirestandtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAndTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestAndTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mms/apirestandtest/ApiRestAndTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mms.apirestandtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAndTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
